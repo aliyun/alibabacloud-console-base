79feb7f25703765e347fc61669a714ccc0013a3c
woota.lj <woota.lj@alibaba-inc.com>
2020-05-14 14:50:15 +0800

    chore(cra-widget): code format.

--- packages/cra-template-widget/template/src/services/DescribeVpcs.js
+++ packages/cra-template-widget/template/src/services/DescribeVpcs.js
@@ -1,12 +1,12 @@
 import createService from './createService'
 
 /**
- * 
+ *
  * 更多关于 createService(productId, action) 函数的用法
  * 可参见同目录下的 ./createService.js 文件
- * 
+ *
  * DescribeVpcs(params)
- * 
+ *
  * @param {object} [params] DescribeVpcs 接口调用参数
  * @param {string} [params.RegionId] 区域 ID，如：cn-hangzhou
  * @param {number} [params.PageNumber=1] 当前页
@@ -16,11 +16,11 @@ import createService from './createService'
  * @param {boolean} [params.IsDefault]
  * @param {boolean} [params.DryRun]
  * @param {string} [params.ResourceGroupId]
- * 
+ *
  * 有关 DescribeVpcs 接口的更多内容参见：https://help.aliyun.com/document_detail/35739.html
- * 
+ *
  * @return {promise} resolve 接口的请求结果
- * 
+ *
  */
 const DescribeVpcs = createService('vpc', 'DescribeVpcs')
 
