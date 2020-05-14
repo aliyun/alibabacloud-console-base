7adc4aa16e9f9e35c0e9b0f2eea7cce87b9befc4
woota.lj <woota.lj@alibaba-inc.com>
2020-05-14 14:50:15 +0800

    chore(cra-widget): code format.

--- packages/cra-template-widget/template/src/services/createService.js
+++ packages/cra-template-widget/template/src/services/createService.js
@@ -2,20 +2,20 @@ import { serviceFactory } from '@alicloud/console-request'
 
 /**
  * createService(productId, action) 是一个创建 OpenAPI 请求工具函数的工厂函数。
- * 
+ *
  * @param {string} [productId] 云产品 ID
  * @param {string} [action] 要执行的操作
  * @return {function} api 请求工具函数
- * 
+ *
  * 使用示例可参考同目录下的 ./DescribeVpcs.js 文件。
- * 
+ *
  */
 const createService = serviceFactory({
   /**
    * 使用本地开发 server 代理 api 请求至
    * public/api/ 目录下的 json 文件
    */
-  apiMockType: 'json-files'
+  apiMockType: 'json-file',
 })
 
 export default createService
