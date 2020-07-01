07cf6148fdfe962deff847a648fc40780facde50
woota.lj <woota.lj@alibaba-inc.com>
2020-07-01 11:42:11 +0800

    feat(build-plugin): First version of widget build plugin.

---
+++ packages/widget-build-webpack-plugin/index.js
@@ -0,0 +1,51 @@
+const { ConcatSource } = require('webpack-sources')
+
+class WidgetBuildWebpackPlugin {
+  constructor(opts = {}) {
+    this.env = opts.env
+    this.globalIdentifier = opts.globalIdentifier || 'widgetJsonp'
+  }
+
+  apply(compiler) {
+    compiler.hooks.compilation.tap(
+      'WidgetBuildWebpackPlugin',
+      (compilation) => {
+        compilation.hooks.optimizeChunkAssets.tap(
+          'WidgetBuildWebpackPlugin',
+          (chunks) => {
+            for (const chunk of chunks) {
+              if (!chunk.canBeInitial()) {
+                // eslint-disable-next-line no-continue
+                continue
+              }
+
+              for (const file of chunk.files) {
+                const targetFile = file === 'index.js'
+
+                if (targetFile) {
+                  compilation.updateAsset(
+                    file,
+                    (old) =>
+                      new ConcatSource(
+                        `(window["${this.globalIdentifier}"] = window["${
+                          this.globalIdentifier
+                        }"] || []).push([${JSON.stringify(
+                          this.env.id
+                        )}, ${JSON.stringify(
+                          this.env.version
+                        )}, function widgetFactory(module, exports, require, window, document, location, history) {`,
+                        old,
+                        '}])'
+                      )
+                  )
+                }
+              }
+            }
+          }
+        )
+      }
+    )
+  }
+}
+
+module.exports = WidgetBuildWebpackPlugin
