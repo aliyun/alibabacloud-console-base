acb89302132c04745cfbe813c1c8c674adc9af9f
Jianchun Wang <justnewbee@gmail.com>
2020-11-27 12:26:17 +0800

    FEAT interceptor error message done

---
+++ packages/console-fetcher-interceptor-res-error-message/src/util/create-interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  ERROR_TIMEOUT,
+  ERROR_NETWORK,
+  ERROR_RESPONSE_STATUS,
+  FetcherConfig,
+  FetcherFnInterceptResponseRejected
+} from '@alicloud/fetcher';
+
+import intl from '../intl';
+
+export default function createInterceptor(): FetcherFnInterceptResponseRejected {
+  return (err: Error, config: FetcherConfig): void => {
+    // @alicloud/fetcher 给出的错误没有国际化 - 因为 fetcher 是最基础的，不想让它跟 console 环境有关，所以这些错误会在这里做对应的国际化
+    switch (err?.name) {
+      case ERROR_NETWORK:
+        err.message = intl('message:error_network');
+        
+        break;
+      case ERROR_TIMEOUT:
+        err.message = intl('message:error_timeout_{n}ms', {
+          n: config.timeout
+        });
+        
+        break;
+      case ERROR_RESPONSE_STATUS:
+        err.message = intl('message:error_response_status');
+        
+        break;
+      default:
+        break;
+    }
+    
+    throw err;
+  };
+}
