bab47a5bd5d12f6fa1a31da5310b0e2636930538
Jianchun Wang <justnewbee@gmail.com>
2020-12-02 16:06:21 +0800

    CHORE 💥 update npmignore

--- packages/shit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-### $1 == the first args to this script
-### usage: script.sh /path/to/dir/
-
-for f in `find . -maxdepth 1 -mindepth 1 -type d`; do
-  echo $f
-  rm $f/jest.config.js
-done
