1faf88aa8481c77553e47e5b814fdb9da5ee68f0
Jianchun Wang <justnewbee@gmail.com>
2020-12-21 12:39:56 +0800

    CHORE scale display in demo

--- packages/console-base-theme/stories/demo-color-scale/index.tsx
+++ packages/console-base-theme/stories/demo-color-scale/index.tsx
@@ -129,12 +129,12 @@ export default function DemoColorScale(): JSX.Element {
   }, [stateSaturation]);
 
   return <>
-    <H1>控制台色盘</H1>
+    <H1>控制台色盘（有偏差...）</H1>
     <P>参考：<a href="https://done.alibaba-inc.com/file/npMfevdSB70K/nraBotmHA4yO7cyO/preview?categoryId=bqTzhdSLVELC">XConsole 色盘设计</a>。控制台色阶基于 HSL 色彩模型 - 色相 Hue / 饱和度 Saturation / 明度 Lightness：</P>
     <List>
-      <><em>Hue</em> 360º 色相，默认每 10º 为一个节点，基础色相共 36 阶</>
-      <><em>Saturation</em> 饱和度范围 0%-100%，默认所有颜色 100% 饱和度</>
-      <><em>Lightness</em> 明度参数 0%-100%，起始为 19%，以 10% 递增，最高明度 89%，共 8 阶</>
+      <><em>Hue</em>（以下横轴）360º 色相，默认每 10º 为一个节点，基础色相共 36 阶</>
+      <><em>Lightness</em>（以下纵轴）明度参数 0%-100%，起始为 19%，以 10% 递增，最高明度 89%，共 8 阶</>
+      <><em>Saturation</em>（XConsole 只选 6 阶，每隔 20%）饱和度范围 0%-100%，默认所有颜色 100% 饱和度</>
     </List>
     <RadioGroup<number> {...{
       label: '饱和度（Saturation）',
