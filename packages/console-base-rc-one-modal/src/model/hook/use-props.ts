1138654456d7bc0d2bb84da1e71e91e7ceba65d7
Jianchun Wang <justnewbee@gmail.com>
2021-03-11 18:04:23 +0800

    CHORE use missing type helpers

--- packages/console-base-rc-one-modal/src/model/hook/use-props.ts
+++ packages/console-base-rc-one-modal/src/model/hook/use-props.ts
@@ -3,6 +3,10 @@ import {
 } from 'react';
 
 import {
+  RequiredBut
+} from '@alicloud/typescript-missing-helpers';
+
+import {
   IPropsModal
 } from '../../types';
 import {
@@ -11,15 +15,7 @@ import {
 
 import useModelContext from './_use-model-context';
 
-// type PartialPick<T, K extends keyof T> = {
-//     [P in K]?: T[P];
-// };
-// type CopyWithPartial<T, K extends keyof T> = Omit<T, K> & Partial<T>;
-// type OptionalBut<T, TRequired extends keyof T> = Partial<T> & Pick<T, TRequired>;
-type RequiredBut<T, TOptional extends keyof T = keyof T> = Partial<Pick<T, TOptional>> & Required<Pick<T, Exclude<keyof T, TOptional>>>;
-
-type TOptional = 'mode' | 'onModeChange' | 'onClose';
-type IModelProps = RequiredBut<IPropsModal, TOptional>;
+type IModelProps = RequiredBut<IPropsModal, 'mode' | 'onModeChange' | 'onClose'>;
 
 const DEFAULT_PROPS: IModelProps = {
   tabs: {
