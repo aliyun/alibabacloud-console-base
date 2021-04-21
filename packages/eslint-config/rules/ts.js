b6a7855723847e94704ec9d0cf7b95add1d9043b
Jianchun Wang <justnewbee@gmail.com>
2021-04-21 18:25:34 +0800

    CHORE typescript eslint config no-unsed-vars error

--- packages/eslint-config/rules/ts.js
+++ packages/eslint-config/rules/ts.js
@@ -2,18 +2,16 @@ module.exports = {
   /**
    * disable eslint base rules so that corresponding @typescript-eslint/xx rules can work without problem
    * 
+   * - https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/indent.md#how-to-use
    * - https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-shadow.md#how-to-use
    * - https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md
-   * - https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/indent.md#how-to-use
+   * - https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md#how-to-use
    */
-  'no-shadow': 'off',
-  'no-use-before-define': 'off', // 必需，否则会误报，见 
   indent: 'off',
+  'no-shadow': 'off',
+  'no-use-before-define': 'off', 
+  'no-unused-vars': 'off',
   // now the @typescript-eslint rules
-  '@typescript-eslint/no-shadow': ['error'],
-  '@typescript-eslint/no-use-before-define': ['error', {
-    ignoreTypeReferences: false
-  }],
   '@typescript-eslint/indent': ['error', 2, {
     SwitchCase: 1,
     ArrayExpression: 1,
@@ -29,6 +27,15 @@ module.exports = {
       body: 1,
       parameters: 2
     }
+  }],
+  '@typescript-eslint/no-shadow': ['error'],
+  '@typescript-eslint/no-use-before-define': ['error', {
+    ignoreTypeReferences: false
+  }],
+  '@typescript-eslint/no-unused-vars': ['error', {
+    vars: 'all',
+    args: 'after-used',
+    ignoreRestSiblings: true
   }],
   '@typescript-eslint/member-delimiter-style': ['error', {
     multiline: {
