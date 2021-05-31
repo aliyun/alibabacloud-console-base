d1f4b35628c35078689fc8d972ffef2f4c624232
Jianchun Wang <justnewbee@gmail.com>
2021-05-31 11:40:39 +0800

    CHORE use fetcher interceptor cachelocal and merger

--- packages/console-fetcher-basic/src/factory/index.ts
+++ packages/console-fetcher-basic/src/factory/index.ts
@@ -2,9 +2,9 @@ import {
   Fetcher,
   createFetcher
 } from '@alicloud/fetcher';
-import interceptBiz from '@alicloud/console-fetcher-interceptor-res-biz';
 import interceptCacheLocal from '@alicloud/fetcher-interceptor-cache-local';
 import interceptMerger from '@alicloud/fetcher-interceptor-merger';
+import interceptBiz from '@alicloud/console-fetcher-interceptor-res-biz';
 import interceptSecurity from '@alicloud/console-fetcher-interceptor-req-security';
 import interceptFecs from '@alicloud/console-fetcher-interceptor-fecs';
 import interceptErrorMessage from '@alicloud/console-fetcher-interceptor-res-error-message';
