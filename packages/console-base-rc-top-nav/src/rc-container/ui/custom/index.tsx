423d1e81e13c3456a9a82a0e97d40e84b0686504
Jianchun Wang <justnewbee@gmail.com>
2021-06-09 18:52:15 +0800

    CHORE style tuning

--- packages/console-base-rc-top-nav/src/rc-container/ui/custom/index.tsx
+++ packages/console-base-rc-top-nav/src/rc-container/ui/custom/index.tsx
@@ -10,10 +10,10 @@ import {
 const ScCustom = styled(Flex)`
   flex: 1;
 `;
-
-const ScCustomInner = styled(ScCustom)`
-  margin: 0 8px;
+const ScCustomL = styled(ScCustom)`
+  margin-right: 8px;
 `;
+const ScCustomR = ScCustom;
 
 /**
  * 自定义区域
@@ -25,7 +25,7 @@ export default function Custom(): JSX.Element {
   } = useProps();
   
   return <ScCustom align="center">
-    <ScCustomInner align="center" justify="flex-start">{customLeft}</ScCustomInner>
-    <ScCustomInner align="center" justify="flex-end">{customRight}</ScCustomInner>
+    <ScCustomL align="center" justify="flex-start">{customLeft}</ScCustomL>
+    <ScCustomR align="center" justify="flex-end">{customRight}</ScCustomR>
   </ScCustom>;
 }
