cc9fa2836d6247ece96666d0318d6ce424fe2399
Jianchun Wang <justnewbee@gmail.com>
2021-09-08 12:08:14 +0800

    FIX doc

--- packages/console-base-log-sls/src/index.ts
+++ packages/console-base-log-sls/src/index.ts
@@ -2,12 +2,12 @@ import createLogger from '@alicloud/console-logger-sls';
 
 import getOptions from './util/get-options';
 
+export * from '@alicloud/console-logger-sls';
+
 // export 出去，在 console-base-fetcher 里会用到
 export const SLS_OPTIONS = getOptions();
 
 /**
- * console-base 专用 logger
+ * console-base 专用 logger TODO 这个包要迁回 @ali
  */
 export default createLogger(SLS_OPTIONS);
-
-export * from '@alicloud/console-logger-sls';
