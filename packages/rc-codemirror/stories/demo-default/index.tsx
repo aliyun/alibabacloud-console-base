86609b8b5e12fc603a937f91783e3230feca65a3
Jianchun Wang <justnewbee@gmail.com>
2021-09-13 15:21:15 +0800

    FEAT add markdown in the default mode list

--- packages/rc-codemirror/stories/demo-default/index.tsx
+++ packages/rc-codemirror/stories/demo-default/index.tsx
@@ -9,7 +9,7 @@ const JSON_STR = JSON.stringify({
   arr: [1, '2', true, {
     boshit: 'is always right'
   }],
-  poet: '丽丽一上床 意思有空日 业火十八禁 充分草于是 - 不要想歪了 - 离离原上草 一岁一枯荣 野火烧不尽 春风吹又生'
+  poet: '离离原上草 一岁一枯荣 野火烧不尽 春风吹又生'
 }, null, 2);
 
 export default function DemoDefault(): JSX.Element {
