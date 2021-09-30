2b2f46e8ac55447ed5b7339f2db01da1f8d1c4a9
Jianchun Wang <justnewbee@gmail.com>
2021-09-30 14:09:19 +0800

    FIX disable user select when moving so browser especially safari will not flash

--- packages/console-base-rc-one-modal/src/model/lifecycle/index.tsx
+++ packages/console-base-rc-one-modal/src/model/lifecycle/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import WindowResize from './window-resize';
 import PushBody from './push-body';
-import BorderDragging from './border-drag';
+import DisableUserSelect from './disable-user-select';
 
 export default function Lifecycle(): JSX.Element {
   return <>
-    <BorderDragging />
     <WindowResize />
     <PushBody />
+    <DisableUserSelect />
   </>;
 }
