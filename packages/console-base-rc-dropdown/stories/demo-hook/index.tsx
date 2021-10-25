6281f01ea912351fb35c9a9ba6f78c55ed03282e
Jianchun Wang <justnewbee@gmail.com>
2021-10-25 20:29:11 +0800

    FIX useDropdown showDrop/hideDrop failed to work when visible is controllable

--- packages/console-base-rc-dropdown/stories/demo-hook/index.tsx
+++ packages/console-base-rc-dropdown/stories/demo-hook/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {
+  useState
+} from 'react';
 
 import {
-  H1
+  H1,
+  H2
 } from '@alicloud/demo-rc-elements';
 
 import Dropdown from '../../src';
@@ -10,11 +13,21 @@ import TheTrigger from './the-trigger';
 import TheDrop from './the-drop';
 
 export default function DemoHook(): JSX.Element {
+  const [stateVisible, setStateVisible] = useState<boolean>(false);
+  
   return <>
     <H1>利用 useDropdown</H1>
+    <H2>visible not controlled</H2>
     <Dropdown {...{
       trigger: <TheTrigger />,
       body: <TheDrop />
+    }} />
+    <H2>visible controlled controlled</H2>
+    <Dropdown {...{
+      visible: stateVisible,
+      trigger: <TheTrigger />,
+      body: <TheDrop />,
+      onVisibleChange: setStateVisible
     }} />
   </>;
 }
