f2abf97ca9f27f77477c941ce1afcc9a2b0ca779
Jianchun Wang <justnewbee@gmail.com>
2021-11-24 12:05:18 +0800

    chore: cannot update stylelint yet

--- packages/console-base-rc-attention-seeker/README.md
+++ packages/console-base-rc-attention-seeker/README.md
@@ -3,7 +3,9 @@
 
 # Usage
 
-建议对需要提示用户关注组件单独写一个组件，注意 ref 必须是 HTML 元素。
+一般不建议直接用组件的形式调用，而是通过 append、prepend、refresh、clear 等方法。
+
+建议对需要提示用户关注的地方单独写一个组件，如下，注意 ref 必须是 HTML 元素。
 
 ```typescript tsx
 import React, {
@@ -12,7 +14,7 @@ import React, {
 } from 'react';
 
 import {
-  put
+  append
 } from '@alicloud/console-base-rc-attention-seeker';
 
 export default function PickMe(): JSX.Element {
@@ -20,10 +22,34 @@ export default function PickMe(): JSX.Element {
   
   useEffect(() => {
     if (refSpan.current) {
-      return put(refSpan.current);
+      return append(refSpan.current);
     }
   }, []);
   
   return <span ref={refSpan}>and i will get the attention</span>;
 }
 ```
+
+# API
+
+TODO 目前没有上一个下一个...
+
+## default export - 组件
+
+基本用不到
+
+## `append(element: HTMLElement, options?: IAttentionSeekerOptions): () => void;`
+
+将元素添加到元素列表，返回解除的无参回调函数可用于 useEffect。
+
+## `prepend(element: HTMLElement, options?: IAttentionSeekerOptions): () => void`
+
+将元素添加到元素列表（如果希望添加后立即高亮，用它），返回解除的无参回调函数可用于 useEffect。
+
+## `clear(): void`
+
+清除所有元素。
+
+## `refresh(): void`
+
+刷新（基本应该用不到了）。
--- packages/console-base-rc-attention-seeker/stories/demo-default/index.tsx
+++ packages/console-base-rc-attention-seeker/stories/demo-default/index.tsx
@@ -21,23 +21,34 @@ const ScFixed = styled.div`
   position: fixed;
   top: 40px;
   right: 20px;
-  padding: 20px;
   z-index: 1234567;
+  padding: 20px;
   background-color: #ffc;
 `;
 
 const ScTheDiv = styled.div`
   padding: 20px;
-  width: 400px;
   border-radius: 12px;
   background: #0064c8;
-  color: #fff;
+  width: 400px;
   text-align: center;
+  color: #fff;
+`;
+
+const ScTheRound = styled.div`
+  border-radius: 50%;
+  background: #c80064;
+  width: 100px;
+  height: 100px;
+  line-height: 100px;
+  text-align: center;
+  color: #fff;
 `;
 
 export default function DemoDefault(): JSX.Element {
   const [stateDomP, setStateDomP] = useState<HTMLElement | null>(null);
   const [stateDomDiv, setStateDomDiv] = useState<HTMLElement | null>(null);
+  const [stateDomRound, setStateDomRound] = useState<HTMLElement | null>(null);
   const [stateDomStrong, setStateDomStrong] = useState<HTMLElement | null>(null);
   const [stateDomButton1, setStateDomButton1] = useState<HTMLElement | null>(null);
   const [stateDomButton2, setStateDomButton2] = useState<HTMLElement | null>(null);
@@ -52,6 +63,11 @@ export default function DemoDefault(): JSX.Element {
       prepend(stateDomDiv);
     }
   }, [stateDomDiv]);
+  const handleRoundAttention = useCallback(() => {
+    if (stateDomRound) {
+      prepend(stateDomRound);
+    }
+  }, [stateDomRound]);
   const handleStrongAttention = useCallback(() => {
     if (stateDomStrong) {
       prepend(stateDomStrong);
@@ -73,16 +89,19 @@ export default function DemoDefault(): JSX.Element {
     <H1>@alicloud/console-base-rc-attention-seeker</H1>
     <Button onClick={handleStrongAttention}>the strong</Button>
     <Button onClick={handleDivAttention}>the div</Button>
+    <Button onClick={handleRoundAttention}>the round</Button>
     <Button onClick={handlePAttention}>the p</Button>
     <Button ref={setStateDomButton1} onClick={handleButton2Attention}>scroll down to the button</Button>
     <ScFixed>
       <Button onClick={handleStrongAttention}>the strong</Button>
       <Button onClick={handleDivAttention}>the div</Button>
+      <Button onClick={handleRoundAttention}>the round</Button>
       <Button onClick={handlePAttention}>the p</Button>
       <Button onClick={clear}>clear</Button>
     </ScFixed>
     <P ref={setStateDomP}>「金麟岂是池中物，一遇风云便化龙。九霄龙吟惊天变，风云际会浅水游。」这是泥菩萨给天下会帮主雄霸的四句批言，概括了他一生的命运。前两句「金麟岂是池中物，一遇风云便化龙」是指前半生雄霸的霸业尽得聂风和步惊云的帮助，因而完成。后两句「九霄龙吟惊天变，风云际会浅水游」则是指雄霸失败，亦是风云所致。所谓： <strong ref={setStateDomStrong}>成也风云，败也风云。</strong></P>
     <ScTheDiv ref={setStateDomDiv}>another div</ScTheDiv>
+    <ScTheRound ref={setStateDomRound}>i am round</ScTheRound>
     <LongArticle />
     <Button ref={setStateDomButton2} onClick={handleButton1Attention}>scroll up to the button</Button>
   </>;
