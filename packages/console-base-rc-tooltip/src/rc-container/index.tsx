7cd7ce5c6e2fe6917825db1aea186e3e5200e31f
Jianchun Wang <justnewbee@gmail.com>
2022-01-06 17:52:19 +0800

    build: mannually pub

--- packages/console-base-rc-tooltip/src/rc-container/index.tsx
+++ packages/console-base-rc-tooltip/src/rc-container/index.tsx
@@ -8,6 +8,11 @@ import Ui from './ui';
 import Lifecycle from './lifecycle';
 
 // TODO 还用不了
+// 参考
+// https://github.com/floating-ui/floating-ui - https://popper.js.org/
+// https://github.com/mohsinulhaq/react-popper-tooltip
+// https://github.com/atomiks/tippyjs - https://atomiks.github.io/tippyjs/
+// https://github.com/wwayne/react-tooltip - https://wwayne.github.io/react-tooltip/
 export default function WithProvider(props: ModelProps): JSX.Element {
   return <Model props={props}>
     <Ui />
