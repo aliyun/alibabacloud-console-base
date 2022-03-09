3fd962ed551c12d3ef572ab9b5d7033b4191e983
Jianchun Wang <justnewbee@gmail.com>
2022-03-09 13:42:01 +0800

    chore: up messenger version

--- packages/console-base-messenger/src/messenger/app-subscribe/toolkit/on-toolkit-tooltip-close.ts
+++ packages/console-base-messenger/src/messenger/app-subscribe/toolkit/on-toolkit-tooltip-close.ts
@@ -10,5 +10,5 @@ import {
  * 某工具的 Tooltip 被关闭，当设置工具的 tooltip.defaultVisible = true，要求加上 closable: true 并监听关闭（关闭后应该不再有 defaultVisible: true）
  */
 export default function onToolkitTooltipClose(id: string, fn: () => void): () => void {
-  return subscribeByApp(composeToolkitTypeWithId(EToolkitTypeShort.TOOL_CLICKED, id), fn);
+  return subscribeByApp(composeToolkitTypeWithId(EToolkitTypeShort.TOOL_TOOLTIP_CLOSE, id), fn);
 }
