025e2ab0916d3d0043725d415caac013b1cc1929
zhenlan-zhao <13102187679@163.com>
2022-03-17 20:26:12 +0800

    FEAT: 修复 security 请求拦截器取 umid 报错的问题

--- packages/console-fetcher-interceptor-req-security/src/util/get-umid.ts
+++ packages/console-fetcher-interceptor-req-security/src/util/get-umid.ts
@@ -7,7 +7,7 @@ import {
  */
 export default function getUmid(): string | undefined {
   try {
-    const umidFromGetToken = (window as IWindow)?.um?.getToken(); // getToken 方法在绑定 g.alicdn.com 的 host 时不存在
+    const umidFromGetToken = (window as IWindow)?.um?.getToken?.(); // getToken 方法在绑定 g.alicdn.com 的 host 时不存在
     const umidFromRiskInfo = (window as IWindow)?.RISK_INFO?.UMID;
 
     return umidFromGetToken || umidFromRiskInfo; // 优先取 getToken 的结果，理论上 umidFromGetToken 和 umidFromRiskInfo 的值是一样的
