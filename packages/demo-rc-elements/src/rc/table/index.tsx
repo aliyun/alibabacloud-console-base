aec18bdfadd65a73feeb6c2f3d8ce5b8c379ec82
Jianchun Wang <justnewbee@gmail.com>
2022-07-08 13:23:53 +0800

    chore: up version ✨

--- packages/demo-rc-elements/src/rc/table/index.tsx
+++ packages/demo-rc-elements/src/rc/table/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {
+  isValidElement
+} from 'react';
 import styled from 'styled-components';
 
 import {
@@ -77,11 +79,26 @@ function getRowKeyFallback<T>(o: T, index: number): string | number {
 }
 
 function renderCell<T>(o: T, index: number, columnProps: TTableColumnProps<T>): JSX.Element | string | null | undefined {
-  if ('dataIndex' in columnProps) {
-    return o[columnProps.dataIndex] as unknown as string;
+  if ('renderCell' in columnProps) {
+    return columnProps.renderCell(o, index);
   }
   
-  return columnProps?.renderCell(o, index);
+  if ('dataIndex' in columnProps) {
+    const value = o[columnProps.dataIndex];
+    
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (value === undefined || value === null) {
+      return null;
+    }
+    
+    if (typeof value === 'string' || isValidElement(value)) {
+      return value;
+    }
+    
+    return String(value);
+  }
+  
+  return null;
 }
 
 function getRowKey<T>(o: T, index: number, primaryKey?: keyof T): string | number {
