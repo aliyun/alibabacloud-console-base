c10ead11228721c89edbef7074ad39807074a8a6
李勇 <ly01390694@alibaba-inc.com>
2022-10-25 15:47:27 +0800

    fix: 添加包裹顶部栏的 DOM 元素

--- packages-rc/console-base-rc-top-nav/src/rc/icon-aliyun/index.tsx
+++ packages-rc/console-base-rc-top-nav/src/rc/icon-aliyun/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import Icon from '@alicloud/console-base-rc-icon';
 import CONF_LOCALE, {
@@ -10,23 +9,11 @@ import {
   useVisibleLogo
 } from '../../model';
 
-const ScIconHolder = styled.span`
-  i:nth-child(2) {
-    margin-left: 4px;
-  }
-  
-  @media screen and (max-width: 900px) {
-    i:nth-child(2) {
-      display: none;
-    }
-  }
-`;
-
 export default function IconAliyun(): JSX.Element {
   const visibleLogo = useVisibleLogo();
 
-  return <ScIconHolder>
+  return <>
     <Icon type="logo-aliyun" />
     {visibleLogo ? <Icon type={CONF_LOCALE.LANGUAGE === ELanguage.ZH ? 'logo-aliyun-word-cn' : 'logo-aliyun-word-en'} /> : null}
-  </ScIconHolder>;
+  </>;
 }
