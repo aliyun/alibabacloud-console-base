58c098702eb83f0d4d314a18a346ffd13f8dd535
李勇 <ly01390694@alibaba-inc.com>
2023-01-11 15:48:36 +0800

    fix: 修改了文件名

--- packages-rc/console-base-rc-aside-nav/src/model/hook/index.ts
+++ packages-rc/console-base-rc-aside-nav/src/model/hook/index.ts
@@ -1,8 +1,8 @@
 export { default as useProps } from './_use-model-props';
 export { default as useCollapsed } from './use-collapsed';
-export { default as useFiltering } from './use-filtering';
 export { default as useItems } from './use-items';
 export { default as useItemsInFooter } from './use-items-in-footer';
+export { default as useFiltering } from './use-filtering';
 export { default as useIsFilter } from './use-is-filter';
 export { default as useIsShowFilteringItems } from './use-is-show-filtering-items';
 // handlers
