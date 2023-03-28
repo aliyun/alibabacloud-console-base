61c230f4699ccd66fefed14efa48159a993ce899
Jianchun Wang <justnewbee@gmail.com>
2023-03-28 16:00:26 +0800

    fix: export types

--- packages-rc/console-base-rc-app-layout/src/index.ts
+++ packages-rc/console-base-rc-app-layout/src/index.ts
@@ -3,3 +3,9 @@ export { default } from './with-model';
 export type {
   ModelProps as AppLayoutProps
 } from './model';
+
+export type {
+  AsideNavProps,
+  AsideNavItemProps,
+  AsideNavItem
+} from '@alicloud/console-base-rc-aside-nav';
